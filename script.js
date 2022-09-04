ed812af96b9f40a5ac32cb682022cb35b1ffd5ec
Slawek01~ <lilcrunker@gmail.com>
2022-09-04 20:38:47 +0200

    added scroll to top button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const scrollToTop = document.querySelector('.scroll-to-top')
-
+const btn = document.querySelector('.scroll-to-top')
 let root = document.documentElement
 
 const scrollBarHandle = () => {
@@ -7,5 +6,22 @@ const scrollBarHandle = () => {
 	const leftToScroll = document.body.getBoundingClientRect().height - window.innerHeight
 	const scrollBarWidth = Math.floor((scroll / leftToScroll) * 100)
 	root.style.setProperty('--scroll-width', `${scrollBarWidth}%`)
+
+    if(scrollBarWidth > 80){
+        btn.classList.add('active')
+    } else{
+        btn.classList.remove('active')
+        
+    }
 }
+
+const scrollToTop = () => {
+    window.scroll({
+        top:0,
+        behavior: 'smooth' 
+    })
+}
+
 window.addEventListener('scroll', scrollBarHandle)
+btn.addEventListener('click', scrollToTop)
+
